4d4112ad1f87b5854dceaf3ff58a23776ef9b174
Carla Oliveira <carlaoliveira.ads@gmail.com>
2021-08-15 10:59:08 -0300

    feat: inicializando o projeto

---
+++ src/test/java/br/com/carla/crudjavaspring/CrudJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carla.crudjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
